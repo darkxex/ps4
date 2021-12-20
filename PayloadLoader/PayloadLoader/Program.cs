01f77376714368d800836e0bef04acc117123e30
Angel Xex <darkxex@gmail.com>
2021-12-20 16:12:03 -0300

    generator

---
+++ PayloadLoader/PayloadLoader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PayloadLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string namefile = args[0];
+                byte[] byteBuffer = File.ReadAllBytes(namefile);
+                string hex = BitConverter.ToString(byteBuffer);
+                string[] newint = hex.Split('-');
+
+                string newpayload = "var payload = [";
+                Console.WriteLine("wait...");
+                foreach (string value in newint)
+                { int modint = Convert.ToInt32("0x" + value, 16); newpayload += (modint + ","); }
+                newpayload = newpayload.Substring(0, newpayload.Length - 1) + "];";
+                Console.WriteLine(newpayload);
+                File.WriteAllText("payload.js", newpayload);
+                
+            }
+        }
+    }
+}
